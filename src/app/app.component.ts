f8b63fecceb595e86a0c23903fc77f965f98a9ee
Imran Khan <imran.khan@kpn.com>
2018-04-14 13:55:51 +0200

    created functional user service

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { UsersService } from './services/users/users.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
+})
+export class AppComponent {
+
+}
